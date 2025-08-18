c18306a3a0040f58e3157acae0cee7e615e27bda
SharmiliVN <sv0498@srmist.edu.in>
2025-08-18 21:34:31 +0530

    Automate Boring Stuff

---
+++ start.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+#chapter 1 
+#int,str funcs
+
+print(int(7.2))
+print(round(7.7778, 1))
+print("what is your age ?")
+age = input('>')
+print("what is your name ?")
+name = input('>')
+print("Your name is " +  name + " and you are " +  str(age) + " years old.")
+print( name + " you will be " + str(int(age) + 1) + " next year")
+
+print(abs(-25))
+
+#use of concatenation and replication
+
+print('bob' + '5')
+print('Alice' * 5)
+print('allice' + 'wonderland')
+print('alice' + 5)
+
